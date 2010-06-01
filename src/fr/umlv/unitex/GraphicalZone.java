a7ccbd45da9ec4d110a9f882ee6d40ad1d1085b4
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2010-06-01 06:58:24 +0000

    Using a stroke to draw graph frames

--- src/fr/umlv/unitex/GraphicalZone.java
+++ src/fr/umlv/unitex/GraphicalZone.java
@@ -25,6 +25,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -438,8 +439,10 @@ public class GraphicalZone extends GenericGraphicalZone implements Printable {
 		f.fillRect(0, 0, getWidth(), getHeight());
 		if (info.frame) {
 			f.setColor(info.foregroundColor);
+			Stroke oldStroke=f.getStroke();
+			f.setStroke(GraphicalToolBox.frameStroke);
 			f.drawRect(10, 10, getWidth() - 20, getHeight() - 20);
-			f.drawRect(9, 9, getWidth() - 18, getHeight() - 18);
+			f.setStroke(oldStroke);
 		}
 		f.setColor(info.foregroundColor);
 		File file = ((GraphFrame) parentFrame).getGraph();
