fac5ff0972da179af80133a2919057d5b785c962
Alexander Orlov <alexander.orlov@loxal.net>
2010-06-20 17:12:07 +0000

    Performance issue: Avoid creation of unnecessary temporary objects => replaced "NEW Integer(intNumber).intValue()" by "Integer.valueOf(intNumber)"

--- src/fr/umlv/unitex/frames/TextFrame.java
+++ src/fr/umlv/unitex/frames/TextFrame.java
@@ -20,23 +20,6 @@
  */
 package fr.umlv.unitex.frames;
 
-import java.awt.BorderLayout;
-import java.awt.ComponentOrientation;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import javax.swing.BorderFactory;
-import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.InternalFrameAdapter;
-import javax.swing.event.InternalFrameEvent;
-
 import fr.umlv.unitex.Config;
 import fr.umlv.unitex.MyDropTarget;
 import fr.umlv.unitex.Preferences;
@@ -45,140 +28,148 @@ import fr.umlv.unitex.io.UnicodeIO;
 import fr.umlv.unitex.listeners.FontListener;
 import fr.umlv.unitex.text.BigTextArea;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * This class describes a frame used to display the current corpus.
- * 
+ *
  * @author Sébastien Paumier
- * 
  */
 public class TextFrame extends JInternalFrame {
-	protected BigTextArea text = new BigTextArea();
-	protected JLabel ligne1 = new JLabel("");
-	protected JLabel ligne2 = new JLabel("");
+    protected BigTextArea text = new BigTextArea();
+    protected JLabel ligne1 = new JLabel("");
+    protected JLabel ligne2 = new JLabel("");
 
-	TextFrame() {
-		super("", true, true, true, true);
-		MyDropTarget.newDropTarget(this);
-		JPanel top = new JPanel(new BorderLayout());
-		top.setOpaque(true);
-		top.setBorder(new EmptyBorder(2, 2, 2, 2));
-		JPanel middle = new JPanel(new BorderLayout());
-		middle.setOpaque(true);
-		middle.setBorder(BorderFactory.createLoweredBevelBorder());
-		middle.add(text);
-		final FontListener fontListener = new FontListener() {
-			public void fontChanged(Font font) {
-				text.setFont(font);
-			}
-		};
-		Preferences.addTextFontListener(fontListener);
-		JPanel up = new JPanel(new GridLayout(2, 1));
-		up.setBorder(new EmptyBorder(2, 2, 2, 2));
-		up.add(ligne1);
-		up.add(ligne2);
-		top.add(up, BorderLayout.NORTH);
-		top.add(middle, BorderLayout.CENTER);
-		setContentPane(top);
-		pack();
-		setBounds(50, 50, 800, 500);
-		addInternalFrameListener(new InternalFrameAdapter() {
-			@Override
-			public void internalFrameClosed(InternalFrameEvent e) {
-				Preferences.removeTextFontListener(fontListener);
-				text.reset();
-				System.gc();
-			}
-		});
-	}
+    TextFrame() {
+        super("", true, true, true, true);
+        MyDropTarget.newDropTarget(this);
+        JPanel top = new JPanel(new BorderLayout());
+        top.setOpaque(true);
+        top.setBorder(new EmptyBorder(2, 2, 2, 2));
+        JPanel middle = new JPanel(new BorderLayout());
+        middle.setOpaque(true);
+        middle.setBorder(BorderFactory.createLoweredBevelBorder());
+        middle.add(text);
+        final FontListener fontListener = new FontListener() {
+            public void fontChanged(Font font) {
+                text.setFont(font);
+            }
+        };
+        Preferences.addTextFontListener(fontListener);
+        JPanel up = new JPanel(new GridLayout(2, 1));
+        up.setBorder(new EmptyBorder(2, 2, 2, 2));
+        up.add(ligne1);
+        up.add(ligne2);
+        top.add(up, BorderLayout.NORTH);
+        top.add(middle, BorderLayout.CENTER);
+        setContentPane(top);
+        pack();
+        setBounds(50, 50, 800, 500);
+        addInternalFrameListener(new InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                Preferences.removeTextFontListener(fontListener);
+                text.reset();
+                System.gc();
+            }
+        });
+    }
 
-	private void loadStatistics() {
-		ligne1.setText("");
-		ligne2.setText("");
-		FileInputStream source;
-		String s;
-		s = UnicodeIO.readFirstLine(new File(Config.getCurrentSntDir(),
-				"stats.n"));
-		if (s != null)
-			ligne1.setText(" " + s);
-		s = UnicodeIO
-				.readFirstLine(new File(Config.getCurrentSntDir(), "dlf.n"));
-		if (s == null)
-			return;
-		int dlf_entries = new Integer(s).intValue();
-		s = UnicodeIO
-				.readFirstLine(new File(Config.getCurrentSntDir(), "dlc.n"));
-		if (s == null)
-			return;
-		int dlc_entries = new Integer(s).intValue();
-		s = UnicodeIO
-				.readFirstLine(new File(Config.getCurrentSntDir(), "err.n"));
-		if (s == null)
-			return;
-		int err_entries = new Integer(s).intValue();
-		File f = new File(Config.getCurrentSntDir(), "stat_dic.n");
-		if (!f.exists()) {
-			return;
-		}
-		if (!f.canRead()) {
-			return;
-		}
-		int simple_total;
-		int compound_total;
-		int err_total;
-		try {
-			source = UnicodeIO.openUnicodeLittleEndianFileInputStream(f);
-			simple_total = new Integer(UnicodeIO.readLine(source)).intValue();
-			compound_total = new Integer(UnicodeIO.readLine(source)).intValue();
-			err_total = new Integer(UnicodeIO.readLine(source)).intValue();
-			source.close();
-		} catch (NotAUnicodeLittleEndianFileException e) {
-			return;
-		} catch (FileNotFoundException e) {
-			return;
-		} catch (IOException e) {
-			return;
-		}
-		s = " " + String.valueOf(simple_total) + " occurrence"
-				+ (simple_total > 1 ? "s" : "") + " (";
-		s = s + String.valueOf(dlf_entries) + " DLF entries) simple word";
-		if (dlf_entries > 1)
-			s = s + "s";
-		s = s + ", ";
-		s = s + String.valueOf(compound_total) + " occurrence"
-				+ (compound_total > 1 ? "s" : "") + " (";
-		s = s + String.valueOf(dlc_entries) + " DLC entries) compound word";
-		if (dlc_entries > 1)
-			s = s + "s";
-		s = s + ", ";
-		s = s + String.valueOf(err_total) + " occurrence"
-				+ (err_total > 1 ? "s" : "") + " (";
-		s = s + String.valueOf(err_entries) + " ERR lines) unknown word";
-		if (err_entries > 1)
-			s = s + "s";
-		ligne2.setText(s);
-	}
+    private void loadStatistics() {
+        ligne1.setText("");
+        ligne2.setText("");
+        FileInputStream source;
+        String s;
+        s = UnicodeIO.readFirstLine(new File(Config.getCurrentSntDir(),
+                "stats.n"));
+        if (s != null)
+            ligne1.setText(" " + s);
+        s = UnicodeIO
+                .readFirstLine(new File(Config.getCurrentSntDir(), "dlf.n"));
+        if (s == null)
+            return;
+        int dlf_entries = Integer.parseInt(s);
+        s = UnicodeIO
+                .readFirstLine(new File(Config.getCurrentSntDir(), "dlc.n"));
+        if (s == null)
+            return;
+        int dlc_entries = Integer.parseInt(s);
+        s = UnicodeIO
+                .readFirstLine(new File(Config.getCurrentSntDir(), "err.n"));
+        if (s == null)
+            return;
+        int err_entries = Integer.parseInt(s);
+        File f = new File(Config.getCurrentSntDir(), "stat_dic.n");
+        if (!f.exists()) {
+            return;
+        }
+        if (!f.canRead()) {
+            return;
+        }
+        int simple_total;
+        int compound_total;
+        int err_total;
+        try {
+            source = UnicodeIO.openUnicodeLittleEndianFileInputStream(f);
+            simple_total = Integer.parseInt(UnicodeIO.readLine(source));
+            compound_total = Integer.parseInt(UnicodeIO.readLine(source));
+            err_total = Integer.parseInt(UnicodeIO.readLine(source));
+            source.close();
+        } catch (NotAUnicodeLittleEndianFileException e) {
+            return;
+        } catch (FileNotFoundException e) {
+            return;
+        } catch (IOException e) {
+            return;
+        }
+        s = " " + String.valueOf(simple_total) + " occurrence"
+                + (simple_total > 1 ? "s" : "") + " (";
+        s = s + String.valueOf(dlf_entries) + " DLF entries) simple word";
+        if (dlf_entries > 1)
+            s = s + "s";
+        s = s + ", ";
+        s = s + String.valueOf(compound_total) + " occurrence"
+                + (compound_total > 1 ? "s" : "") + " (";
+        s = s + String.valueOf(dlc_entries) + " DLC entries) compound word";
+        if (dlc_entries > 1)
+            s = s + "s";
+        s = s + ", ";
+        s = s + String.valueOf(err_total) + " occurrence"
+                + (err_total > 1 ? "s" : "") + " (";
+        s = s + String.valueOf(err_entries) + " ERR lines) unknown word";
+        if (err_entries > 1)
+            s = s + "s";
+        ligne2.setText(s);
+    }
 
-	/**
-	 * Loads a corpus
-	 * 
-	 * @param sntFile
-	 *            name of the corpus file
-	 */
-	void loadText(File sntFile) {
-		loadStatistics();
-		text.setFont(Config.getCurrentTextFont());
-		text
-				.setComponentOrientation(Config.isRightToLeftLanguage() ? ComponentOrientation.RIGHT_TO_LEFT
-						: ComponentOrientation.LEFT_TO_RIGHT);
-		if (sntFile.length() <= 2) {
-			text.setText(Config.EMPTY_FILE_MESSAGE);
-		} else {
-			text.load(sntFile);
-		}
-		setTitle(sntFile.getAbsolutePath());
-	}
+    /**
+     * Loads a corpus
+     *
+     * @param sntFile name of the corpus file
+     */
+    void loadText(File sntFile) {
+        loadStatistics();
+        text.setFont(Config.getCurrentTextFont());
+        text
+                .setComponentOrientation(Config.isRightToLeftLanguage() ? ComponentOrientation.RIGHT_TO_LEFT
+                        : ComponentOrientation.LEFT_TO_RIGHT);
+        if (sntFile.length() <= 2) {
+            text.setText(Config.EMPTY_FILE_MESSAGE);
+        } else {
+            text.load(sntFile);
+        }
+        setTitle(sntFile.getAbsolutePath());
+    }
 
-	public BigTextArea getText() {
-		return text;
-	}
+    public BigTextArea getText() {
+        return text;
+    }
 }
