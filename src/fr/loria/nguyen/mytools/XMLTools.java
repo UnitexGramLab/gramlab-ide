d27577c22a3db2a54991a152ba6c1344d1d05680
Alexander Orlov <alexander.orlov@loxal.net>
2010-06-24 01:12:49 +0000

    Performance issue: Replaced redundant String constructor String s = new String("string") by String s = "string"

--- src/fr/loria/nguyen/mytools/XMLTools.java
+++ src/fr/loria/nguyen/mytools/XMLTools.java
@@ -30,8 +30,6 @@
  */
 package fr.loria.nguyen.mytools;
 
-import java.io.FileOutputStream;
-
 import org.apache.xerces.dom.DocumentImpl;
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
@@ -40,151 +38,142 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
+import java.io.FileOutputStream;
+
 public class XMLTools {
 
     public static final String STD_HEADER
-	= "<?xml version=\"1.0\" encoding = \"UTF-8\"?>\n\n";
-    public static final String parserClass =  "org.apache.xerces.parsers.SAXParser";
+            = "<?xml version=\"1.0\" encoding = \"UTF-8\"?>\n\n";
+    public static final String parserClass = "org.apache.xerces.parsers.SAXParser";
 
-    
+
     public static String getTextValue(Node node) {
-	Node item = node.getFirstChild();
-	while (item != null && item.getNodeType() != Node.TEXT_NODE)
-	    item = item.getNextSibling();
-	if(item == null) 
-	    return "";
-	return new String(item.getNodeValue());
+        Node item = node.getFirstChild();
+        while (item != null && item.getNodeType() != Node.TEXT_NODE)
+            item = item.getNextSibling();
+        if (item == null)
+            return "";
+        return item.getNodeValue();
     }
 
     public static void saveDocument(String fileName, Document document, String enc) {
-	try {
-	    OutputFormat format = new OutputFormat(document);
-	    //format.setLineWidth(70);
-	    format.setIndenting(true);
-	    format.setIndent(3);
-	    format.setEncoding(enc);
-	    //format.setDoctype("SYSTEM", "token.dtd"); 
-	    format.setMediaType("application/xml");
-	    format.setOmitComments(true);
-	    format.setOmitXMLDeclaration(false);
-	    format.setVersion("1.0");
-	    format.setStandalone(false);
+        try {
+            OutputFormat format = new OutputFormat(document);
+            //format.setLineWidth(70);
+            format.setIndenting(true);
+            format.setIndent(3);
+            format.setEncoding(enc);
+            //format.setDoctype("SYSTEM", "token.dtd");
+            format.setMediaType("application/xml");
+            format.setOmitComments(true);
+            format.setOmitXMLDeclaration(false);
+            format.setVersion("1.0");
+            format.setStandalone(false);
 
-	    FileOutputStream fileOutStrm = new FileOutputStream(fileName);
-	    XMLSerializer ser = new XMLSerializer(fileOutStrm , format);
-	    ser.serialize(document);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+            FileOutputStream fileOutStrm = new FileOutputStream(fileName);
+            XMLSerializer ser = new XMLSerializer(fileOutStrm, format);
+            ser.serialize(document);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String toString(DocumentImpl xdoc) {
-	String ret = null;
-	text = "";
-	walk(xdoc);
-	ret = text;
-	text = null;
-	return ret;
+        String ret = null;
+        text = "";
+        walk(xdoc);
+        ret = text;
+        text = null;
+        return ret;
     }
 
     public static String text = null;
 
-    public static String getText(Node node)   {
-	String ret = null;
-	text = "";
-	walkText(node);
-	ret = text;
-	text = null;
-	return ret;
+    public static String getText(Node node) {
+        String ret = null;
+        text = "";
+        walkText(node);
+        ret = text;
+        text = null;
+        return ret;
     }
-    
-    public static void  walkText(Node node)    {
+
+    public static void walkText(Node node) {
         int type = node.getNodeType();
 
-	if (type == Node.TEXT_NODE)
-	    text += node.getNodeValue();
+        if (type == Node.TEXT_NODE)
+            text += node.getNodeValue();
 
         //recurse
-        for(Node child = node.getFirstChild(); child != null; child = child.getNextSibling())
-        {
+        for (Node child = node.getFirstChild(); child != null; child = child.getNextSibling()) {
             walkText(child);
         }
-        
+
     }//end of walkText
-  
-    public static void  walk(Node node)    {
+
+    public static void walk(Node node) {
         int type = node.getNodeType();
-        switch(type)
-        {
-            case Node.ELEMENT_NODE:
-            {
+        switch (type) {
+            case Node.ELEMENT_NODE: {
                 text += '<' + node.getNodeName();
                 NamedNodeMap nnm = node.getAttributes();
-                if(nnm != null )
-                {
-                    int len = nnm.getLength() ;
+                if (nnm != null) {
+                    int len = nnm.getLength();
                     Attr attr;
-                    for ( int i = 0; i < len; i++ )
-                    {
-                        attr = (Attr)nnm.item(i);
-                        text += ' ' 
-                             + attr.getNodeName()
-                             + "=\""
-                             + attr.getNodeValue()
-                             +  '"';
+                    for (int i = 0; i < len; i++) {
+                        attr = (Attr) nnm.item(i);
+                        text += ' '
+                                + attr.getNodeName()
+                                + "=\""
+                                + attr.getNodeValue()
+                                + '"';
                     }
                 }
                 text += '>';
-                
+
                 break;
-                
+
             }//end of element
-            case Node.ENTITY_REFERENCE_NODE:
-            {
-               
-               text += '&' + node.getNodeName() + ';' ;
-               break;
-                
+            case Node.ENTITY_REFERENCE_NODE: {
+
+                text += '&' + node.getNodeName() + ';';
+                break;
+
             }//end of entity
-            case Node.CDATA_SECTION_NODE:
-            {
-                    text +=  "<![CDATA[" 
-                            + node.getNodeValue()
-                            + "]]>" ;
-                     break;       
-                
+            case Node.CDATA_SECTION_NODE: {
+                text += "<![CDATA["
+                        + node.getNodeValue()
+                        + "]]>";
+                break;
+
             }
-            case Node.TEXT_NODE:
-            {
+            case Node.TEXT_NODE: {
                 text += node.getNodeValue();
                 break;
             }
-            case Node.PROCESSING_INSTRUCTION_NODE:
-            {
-                text += "<?" 
-                    + node.getNodeName();
+            case Node.PROCESSING_INSTRUCTION_NODE: {
+                text += "<?"
+                        + node.getNodeName();
                 String data = node.getNodeValue();
-                if ( data != null && data.length() > 0 ) {
-                    text += ' ' + data ;
+                if (data != null && data.length() > 0) {
+                    text += ' ' + data;
                 }
                 text += "?>";
                 break;
 
-             }
+            }
         }//end of switch
-        
-              
+
+
         //recurse
-        for(Node child = node.getFirstChild(); child != null; child = child.getNextSibling())
-        {
+        for (Node child = node.getFirstChild(); child != null; child = child.getNextSibling()) {
             walk(child);
         }
-        
+
         //without this the ending tags will miss
-        if ( type == Node.ELEMENT_NODE )
-        {
+        if (type == Node.ELEMENT_NODE) {
             text += "</" + node.getNodeName() + ">";
         }
-       
+
     }//end of walk
 }
