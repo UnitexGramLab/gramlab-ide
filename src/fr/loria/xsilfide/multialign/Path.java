7cccbda86b3803925b6fec3d8665078828e5da62
Alexander Orlov <alexander.orlov@loxal.net>
2010-06-24 01:12:49 +0000

    Performance issue: Replaced redundant String constructor String s = new String("string") by String s = "string"

--- src/fr/loria/xsilfide/multialign/Path.java
+++ src/fr/loria/xsilfide/multialign/Path.java
@@ -36,58 +36,51 @@ package fr.loria.xsilfide.multialign;
 
 import java.util.Vector;
 
-@SuppressWarnings("unchecked") 
-public class Path
-{
-  protected int cur;        // index of current point
-  protected int num_points; // number of points
-  protected int nx;         // max x
-  protected int ny;         // max y
-  protected Vector path;    // set of points
+@SuppressWarnings("unchecked")
+public class Path {
+    protected int cur;        // index of current point
+    protected int num_points; // number of points
+    protected int nx;         // max x
+    protected int ny;         // max y
+    protected Vector path;    // set of points
 
-  public Path (int x, int y, int size)
-    {
-      num_points = size;
-      nx = x;
-      ny = y;
-      cur = 0;
-      path = new Vector(size);
+    public Path(int x, int y, int size) {
+        num_points = size;
+        nx = x;
+        ny = y;
+        cur = 0;
+        path = new Vector(size);
     }
 
-  public int getNumberOfPoint()
-    {
-      return(this.path.size());
+    public int getNumberOfPoint() {
+        return (this.path.size());
     }
 
-  public void setPointAt (Point p, int at)
-    {
-      cur = at;
-      path.insertElementAt(p, at);    
+    public void setPointAt(Point p, int at) {
+        cur = at;
+        path.insertElementAt(p, at);
     }
-  
-  public void setPointAt (int i, int j, int at)
-    {
-      Point point;
-      
-      point = new Point(i, j);
-      this.setPointAt(point, at);
+
+    public void setPointAt(int i, int j, int at) {
+        Point point;
+
+        point = new Point(i, j);
+        this.setPointAt(point, at);
     }
-  
-  public Point getPointAt(int at)
-    {
-      return((Point)path.elementAt(at));
+
+    public Point getPointAt(int at) {
+        return ((Point) path.elementAt(at));
     }
-  
-  @Override
-public String toString ()
-    {
-      String s = new String ();
-      
-      for (int i = 0; i < this.path.size(); i++ ) {
-	s = s + this.getPointAt(i).toString();
-      }
-      return(s);
-  }
+
+    @Override
+    public String toString() {
+        String s = "";
+
+        for (int i = 0; i < this.path.size(); i++) {
+            s = s + this.getPointAt(i).toString();
+        }
+        return (s);
+    }
 }
 
 // EOF
