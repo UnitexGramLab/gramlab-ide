b2399de143841667924b70d5e118421d6f3fe55f
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2010-09-17 10:27:16 +0000

    Adding transliteration frame

---
+++ src/fr/umlv/unitex/frames/TransliterationFrame.java
@@ -0,0 +1,177 @@
+/*
+ * Unitex
+ *
+ * Copyright (C) 2001-2010 Université Paris-Est Marne-la-Vallée <unitex@univ-mlv.fr>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ *
+ */
+
+package fr.umlv.unitex.frames;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import fr.umlv.unitex.Config;
+import fr.umlv.unitex.exceptions.InvalidDestinationEncodingException;
+import fr.umlv.unitex.exceptions.InvalidSourceEncodingException;
+import fr.umlv.unitex.process.Launcher;
+import fr.umlv.unitex.process.ToDo;
+import fr.umlv.unitex.process.commands.ConvertCommand;
+
+/**
+ * @author Sébastien Paumier
+ */
+public class TransliterationFrame extends JInternalFrame {
+
+    private final JRadioButton DELAS = new JRadioButton("DELAS/DELAC");
+    private final JRadioButton DELAF = new JRadioButton("DELAF/DELACF", true);
+
+    private final JRadioButton srcArabic = new JRadioButton("Arabic",true);
+    private final JRadioButton srcBuckwalter = new JRadioButton("Buckwalter");
+    private final JRadioButton srcBuckwalterPlusPlus = new JRadioButton("Buckwalter++");
+
+    private final JRadioButton destArabic = new JRadioButton("Arabic");
+    private final JRadioButton destBuckwalter = new JRadioButton("Buckwalter");
+    private final JRadioButton destBuckwalterPlusPlus = new JRadioButton("Buckwalter++",true);
+
+    TransliterationFrame() {
+        super("Transliteratation", false, true);
+        setContentPane(constructPanel());
+        pack();
+    }
+
+    private JPanel constructPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(constructLeftPanel(), BorderLayout.WEST);
+        panel.add(constructRightPanel(), BorderLayout.CENTER);
+        JPanel panel2 = new JPanel(new BorderLayout());
+        panel2.add(constructEncodingPanel(),BorderLayout.CENTER);
+        panel2.add(panel,BorderLayout.SOUTH);
+        return panel2;
+    }
+
+    private JPanel constructEncodingPanel() {
+    	JPanel p=new JPanel(new GridLayout(1,2));
+    	JPanel src=new JPanel(new GridLayout(3,1));
+    	src.setBorder(BorderFactory.createTitledBorder("Input"));
+    	ButtonGroup bgSrc=new ButtonGroup();
+    	src.add(srcArabic);
+    	src.add(srcBuckwalter);
+    	src.add(srcBuckwalterPlusPlus);
+    	bgSrc.add(srcArabic);
+    	bgSrc.add(srcBuckwalter);
+    	bgSrc.add(srcBuckwalterPlusPlus);
+    	p.add(src);
+    	
+    	JPanel dest=new JPanel(new GridLayout(3,1));
+    	dest.setBorder(BorderFactory.createTitledBorder("Output"));
+    	ButtonGroup bgDest=new ButtonGroup();
+    	dest.add(destArabic);
+    	dest.add(destBuckwalter);
+    	dest.add(destBuckwalterPlusPlus);
+    	bgDest.add(destArabic);
+    	bgDest.add(destBuckwalter);
+    	bgDest.add(destBuckwalterPlusPlus);
+    	p.add(dest);
+    	return p;
+	}
+
+	private JPanel constructLeftPanel() {
+        JPanel leftPanel = new JPanel();
+        JPanel tmp = new JPanel(new GridLayout(2, 1));
+        tmp.setBorder(new TitledBorder("Dictionary Type:"));
+        DELAS.setSelected(false);
+        DELAF.setSelected(true);
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(DELAS);
+        bg.add(DELAF);
+        tmp.add(DELAS);
+        tmp.add(DELAF);
+        leftPanel.add(tmp);
+        return leftPanel;
+    }
+
+    private JPanel constructRightPanel() {
+        JPanel rightPanel = new JPanel(new GridLayout(2, 1));
+        rightPanel.setBorder(new EmptyBorder(12, 5, 5, 7));
+        Action goAction = new AbstractAction("Transliterate") {
+            public void actionPerformed(ActionEvent arg0) {
+                setVisible(false);
+                transliterate();
+            }
+        };
+        JButton GO = new JButton(goAction);
+        Action cancelAction = new AbstractAction("Cancel") {
+            public void actionPerformed(ActionEvent arg0) {
+                setVisible(false);
+            }
+        };
+        JButton CANCEL = new JButton(cancelAction);
+        rightPanel.add(GO);
+        rightPanel.add(CANCEL);
+        return rightPanel;
+    }
+
+    /**
+     * Launches the <code>CheckDic</code> verification program, through the
+     * creation of a <code>ProcessInfoFrame</code> object.
+     */
+    void transliterate() {
+        ConvertCommand command = new ConvertCommand().file(
+                Config.getCurrentDELA()).replace();
+        if (DELAF.isSelected()) command=command.delaf();
+        else command=command.delas();
+
+		try {
+			if (srcArabic.isSelected()) command=command.src("UTF16LE");
+			else if (srcBuckwalter.isSelected()) command=command.src("buckwalter");
+			else command=command.src("buckwalter++");
+		} catch (InvalidSourceEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			if (destArabic.isSelected()) command=command.dest("UTF16LE");
+			else if (destBuckwalter.isSelected()) command=command.dest("buckwalter");
+			else command=command.dest("buckwalter++");
+		} catch (InvalidDestinationEncodingException e) {
+			e.printStackTrace();
+		}
+
+        setVisible(false);
+        Launcher.exec(command.getBuilder(), true, new ReloadDicDo());
+    }
+
+    class ReloadDicDo implements ToDo {
+        
+    	public void toDo() {
+            UnitexFrame.getFrameManager().newDelaFrame(Config.getCurrentDELA());
+        }
+	}
+
+}
