c8bd515fac7adb0ebdf73a33fdb9d8b703ca5311
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2011-09-07 08:08:27 +0000

    Using a clean singleton for the project manager

--- src/fr/gramlab/workspace/ProjectManager.java
+++ src/fr/gramlab/workspace/ProjectManager.java
@@ -9,24 +9,26 @@ import fr.gramlab.config.GramlabConfigManager;
 
 public class ProjectManager {
 	
-	private static ArrayList<ProjectListener> projectListeners=new ArrayList<ProjectListener>();
-	private static boolean firingProject=false;
+	private ArrayList<ProjectListener> projectListeners=new ArrayList<ProjectListener>();
+	private boolean firingProject=false;
 	
-	private static ArrayList<Project> projects=new ArrayList<Project>();
+	private ArrayList<Project> projects=new ArrayList<Project>();
+	
+	private static ProjectManager manager;
 	
 	
-	public static void addProjectListener(ProjectListener l) {
+	public void addProjectListener(ProjectListener l) {
 		projectListeners.add(l);
 	}
 	
-	public static void removeProjectListener(ProjectListener l) {
+	public void removeProjectListener(ProjectListener l) {
 		if (firingProject) {
 			throw new IllegalStateException("Cannot remove a listener while firing");
 		}
 		projectListeners.remove(l);
 	}
 	
-	protected static void fireProjectAdded(Project p,int pos) {
+	protected void fireProjectAdded(Project p,int pos) {
 		firingProject=true;
 		try {
 			for (ProjectListener l:projectListeners) {
@@ -37,7 +39,7 @@ public class ProjectManager {
 		}
 	}
 
-	protected static void fireProjectRemoved(Project p,int pos) {
+	protected void fireProjectRemoved(Project p,int pos) {
 		firingProject=true;
 		try {
 			for (ProjectListener l:projectListeners) {
@@ -48,7 +50,7 @@ public class ProjectManager {
 		}
 	}
 
-	protected static void fireProjectOpened(Project p,int pos) {
+	protected void fireProjectOpened(Project p,int pos) {
 		firingProject=true;
 		try {
 			for (ProjectListener l:projectListeners) {
@@ -59,7 +61,7 @@ public class ProjectManager {
 		}
 	}
 
-	protected static void fireProjectClosed(Project p,int pos) {
+	protected void fireProjectClosed(Project p,int pos) {
 		firingProject=true;
 		try {
 			for (ProjectListener l:projectListeners) {
@@ -71,7 +73,7 @@ public class ProjectManager {
 	}
 
 	
-	public static void addProject(Project p) {
+	public void addProject(Project p) {
 		if (projects.contains(p)) return;
 		projects.add(p);
 		Collections.sort(projects);
@@ -79,45 +81,53 @@ public class ProjectManager {
 		fireProjectAdded(p,pos);
 	}
 
-	public static void openProject(Project p) {
+	public void openProject(Project p) {
 		int pos=projects.indexOf(p);
 		if (pos==-1) return;
 		p.open();
 		fireProjectOpened(p,pos);
 	}
 
-	public static void closeProject(Project p) {
+	public void closeProject(Project p) {
 		int pos=projects.indexOf(p);
 		if (pos==-1) return;
 		p.close();
 		fireProjectClosed(p,pos);
 	}
 
-	public static void removeProject(Project p) {
+	public void removeProject(Project p) {
 		int pos=projects.indexOf(p);
 		if (pos==-1) return;
 		projects.remove(p);
 		fireProjectRemoved(p,pos);
 	}
 	
-	public static boolean isProjectDirectory(File f) {
+	public boolean isProjectDirectory(File f) {
 		if (!f.exists() || !f.isDirectory() 
 				|| !f.getParentFile().equals(GramlabConfigManager.getWorkspaceDirectory())) return false;
 		Project p=new Project(f.getName());
 		return p.getConfigFile().exists();
 	}
 
-	public static ArrayList<Project> getProjects() {
+	public ArrayList<Project> getProjects() {
 		return projects;
 	}
 
 	
-	public static void loadProjects() {
+	public void loadProjects() {
 		File[] files=GramlabConfigManager.getWorkspaceDirectory().listFiles();
 		for (File f:files) {
 			if (isProjectDirectory(f)) {
 				addProject(new Project(f.getName()));
 			}
 		}
+	}
+
+	public static void setManager(ProjectManager manager) {
+		ProjectManager.manager = manager;
+	}
+
+	public static ProjectManager getManager() {
+		return manager;
 	}
 }
