706b6d5baeb98fb47ac164602f1c49739573762f
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2012-04-03 08:10:37 +0000

    Change for Gramlab compatibility

--- src/fr/umlv/unitex/frames/GraphFrame.java
+++ src/fr/umlv/unitex/frames/GraphFrame.java
@@ -32,6 +32,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentListener;
 import java.awt.image.BufferedImage;
+import java.beans.PropertyVetoException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
