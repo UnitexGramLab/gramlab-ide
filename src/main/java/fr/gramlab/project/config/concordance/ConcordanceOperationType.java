6f424f69cc76cc15f67900c14fc3ebc753b07e77
Mukarr <mukarramtailor@outlook.com>
2016-08-05 07:25:08 +0530

    merging

--- src/main/java/fr/gramlab/project/config/concordance/ConcordanceOperationType.java
+++
@@ -1,7 +0,0 @@
-package fr.gramlab.project.config.concordance;
-
-public enum ConcordanceOperationType {
-	SHOW_DIFFERENCES_WITH_PREVIOUS_CONCORDANCE,
-	SHOW_AMBIGUOUS_OUTPUTS,
-	BUILD_CONCORDANCE
-}
--- src/main/java/fr/gramlab/project/config/maven/PackageOperation.java
+++
@@ -1,7 +0,0 @@
-package fr.gramlab.project.config.maven;
-
-public enum PackageOperation {
-	JUST_BUILD,
-	BUILD_AND_INSTALL,
-	BUILD_AND_DEPLOY
-}
