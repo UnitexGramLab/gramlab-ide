851aab87eeb3c34abd4282f28a0d3197bcf6070f
markpower <rewopkram@tpg.com.au>
2017-02-03 09:19:48 +0800

    moved openGraphFiles() from Config

--- unitex/src/fr/umlv/unitex/Unitex.java
+++ unitex/src/fr/umlv/unitex/Unitex.java
@@ -26,6 +26,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -45,10 +46,12 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.metal.OceanTheme;
 
+import fr.umlv.unitex.common.project.manager.GlobalProjectManager;
 import fr.umlv.unitex.config.Config;
 import fr.umlv.unitex.config.ConfigManager;
 import fr.umlv.unitex.config.Preferences;
 import fr.umlv.unitex.exceptions.UnitexUncaughtExceptionHandler;
+import fr.umlv.unitex.frames.InternalFrameManager;
 import fr.umlv.unitex.frames.SplashScreen;
 import fr.umlv.unitex.frames.UnitexFrame;
 
@@ -101,7 +104,7 @@ public class Unitex {
 				}
 			});
 		} catch (BindException e) {
-	        clientSocketListener(args);
+			clientSocketListener(args);
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
@@ -175,7 +178,7 @@ public class Unitex {
 								frame.setVisible(true);
 								ConfigManager.getManager().getSvnMonitor(null)
 										.start();
-								Config.openGraphFiles(args);
+								openGraphFiles(args);
 							}
 						});
 						timer.stop();
@@ -197,6 +200,21 @@ public class Unitex {
 		}
 	}
 	
+	/**
+     * Extracts the graph files from args passed to application on startup and opens graph frames
+     * Issue #27
+     */
+    public static void openGraphFiles(String[] args) {
+    	for (int i = 0; i < args.length; i++) {
+    		if (args[i].indexOf("=") == -1) {
+    			File f = new File(args[i]);
+    			GlobalProjectManager.search(null)
+				.getFrameManagerAs(InternalFrameManager.class)
+				.newGraphFrame(f);
+    		}		
+    	}
+    }
+	
 	// Server socket - listens for connection from failed instance startup
 	public static void serverSocketListener() { 
 		try {
@@ -206,7 +224,7 @@ public class Unitex {
 			String str = in.readLine();
 			
 			if (str != null && str.length() > 0) {  
-				Config.openGraphFiles(stringToArray(str, DELIMITER));
+				openGraphFiles(stringToArray(str, DELIMITER));
 				if (UnitexFrame.mainFrame.isVisible()) {
 					UnitexFrame.mainFrame.setVisible(true);
 				}
