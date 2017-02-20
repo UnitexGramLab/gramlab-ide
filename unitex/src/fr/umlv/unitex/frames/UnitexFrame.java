d1f8780dc206a4f2aaa89d3d7033d824d3b45cdd
Maxime Petit <phmz@users.noreply.github.com>
2017-02-20 15:53:08 +0100

    [feature #38] Add option to save a .snt corpus as
    This commit add an option save the current .snt file and the 
    corresponding _snt directory with its contents in another 
    directory than corpus. The new snt is set as the current corpus.
    
    Closes: #38

--- unitex/src/fr/umlv/unitex/frames/UnitexFrame.java
+++ unitex/src/fr/umlv/unitex/frames/UnitexFrame.java
@@ -78,7 +78,6 @@ import fr.umlv.unitex.config.PreferencesManager;
 import fr.umlv.unitex.config.SntFileEntry;
 import fr.umlv.unitex.editor.FileEditionMenu;
 import fr.umlv.unitex.files.FileUtil;
-import fr.umlv.unitex.graphrendering.GenericGraphBox;
 import fr.umlv.unitex.graphrendering.GraphMenuBuilder;
 import fr.umlv.unitex.grf.GraphPresentationInfo;
 import fr.umlv.unitex.io.Encoding;
@@ -171,6 +170,7 @@ public class UnitexFrame extends JFrame {
 						convertFst.setEnabled(true);
 						exportTfstAsCsv.setEnabled(true);
 						closeText.setEnabled(true);
+            saveAsSnt.setEnabled(true);
 						File snt = ConfigManager.getManager().getCurrentSnt(
 								null);
 						final File sntDir = FileUtil.getSntDir(snt);
@@ -200,6 +200,7 @@ public class UnitexFrame extends JFrame {
 						convertFst.setEnabled(false);
 						exportTfstAsCsv.setEnabled(false);
 						closeText.setEnabled(false);
+            saveAsSnt.setEnabled(false);
 						GlobalProjectManager.search(null).getFrameManagerAs(InternalFrameManager.class)
 								.closeTokensFrame();
 						GlobalProjectManager.search(null).getFrameManagerAs(InternalFrameManager.class)
@@ -332,6 +333,7 @@ public class UnitexFrame extends JFrame {
 	Action changeLang;
 	Action applyLexicalResources;
 	Action locatePattern;
+  Action saveAsSnt;
 	AbstractAction displayLocatedSequences;
 	AbstractAction elagComp;
 	AbstractAction constructFst;
@@ -442,7 +444,49 @@ public class UnitexFrame extends JFrame {
 			}
 		});
 		textMenu.add(openRecent);
-
+    saveAsSnt = new AbstractAction("Save As...") {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (Config.getCurrentSnt() == null || Config.getCurrentSntDir() == null) {
+          return;
+        }
+        JFileChooser fc = Config.getCorpusDialogBox();
+        fc.setMultiSelectionEnabled(false);
+        fc.setDialogType(JFileChooser.SAVE_DIALOG);
+        File file;
+        for(;;) {
+          final int returnVal = fc.showSaveDialog(UnitexFrame.mainFrame);
+          if (returnVal != JFileChooser.APPROVE_OPTION) {
+            return;
+          }
+          file = fc.getSelectedFile();
+          final String name = file.getAbsolutePath();
+          if (!name.endsWith(".snt")) {
+            file = new File(name + ".snt");
+          }
+          if (file == null || !file.exists()) {
+            break;
+          }
+          final String message = file + "\nalready exists. Do you want to replace it?";
+          final String[] options = {"Yes", "No"};
+          final int n = JOptionPane.showOptionDialog(null, message, "Error", JOptionPane.YES_NO_OPTION, JOptionPane
+            .ERROR_MESSAGE, null, options, options[0]);
+          if (n == 0) {
+            break;
+          }
+        }
+        if (file == null) {
+          return;
+        }
+        FileUtil.copyFile(Config.getCurrentSnt(), file);
+        String folderPath = file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf('.'));
+        File folder = new File(folderPath + "_snt");
+        FileUtil.copyDirRec(Config.getCurrentSntDir(), folder);
+        Text.loadSnt(file, false);
+      }
+    };
+    saveAsSnt.setEnabled(false);
+    textMenu.add(new JMenuItem(saveAsSnt));
 		preprocessText = new AbstractAction("Preprocess Text...") {
 			@Override
 			public void actionPerformed(ActionEvent e) {
