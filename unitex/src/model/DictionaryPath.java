da4bcc5831deed75bf3073ceaf39b30369533d96
anasaitcheikh <aitcheikhanas@gmail.com>
2018-04-27 10:58:58 +0200

    Fixing DictionaryPath

--- unitex/src/model/DictionaryPath.java
+++ unitex/src/model/DictionaryPath.java
@@ -29,18 +29,18 @@ import fr.umlv.unitex.config.*;
  */
 public class DictionaryPath {
     public static ArrayList<String> dictionnary= new ArrayList<>();
-    public static final String allDelafAbsPath = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//Dela//";
+    public static final String allDelafAbsPath = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage()+"//Dela//";
 
-    public static final String delafTmpPathDelac = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//"+"text.txt";
-    public static final String delafTmpAbsPathDelac = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//";
-    public static final String text_sntAbsPath = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//"+"text_snt//dlf";
-    public static final String allDela = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null);
-    public static final String ruleCompoundsPath = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//"+"CompoundsStrat7_Ver5-3.xml";
+    public static final String delafTmpPathDelac = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage()+"//"+"text.txt";
+    public static final String delafTmpAbsPathDelac = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage()+"//";
+    public static final String text_sntAbsPath = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage()+"//"+"text_snt//dlf";
+    public static final String allDela = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage();
+    public static final String ruleCompoundsPath = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage()+"//"+"CompoundsStrat7_Ver5-3.xml";
     public static final String unitexLoggerPath = Config.getUnitexToolLogger().getAbsolutePath();
-    public static final String alphabetPath = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//"+"Alphabet.txt";
-    public static final String inflectionPath = Config.getUserDir().toString().replace("/", "//")+"//"+ConfigManager.getManager().getAlphabet(null)+"//"+"Inflection//";
-    public static final String allDelas = Config.getUserDir()+"/"+ConfigManager.getManager().getAlphabet(null)+"/"+"Dela/Delas/";
-    public static final String allDelac = Config.getUserDir()+"/"+ConfigManager.getManager().getAlphabet(null)+"/"+"Dela/Delac/";
+    public static final String alphabetPath = ConfigManager.getManager().getAlphabet(null).getAbsolutePath();
+    public static final String inflectionPath = Config.getUserDir().toString().replace("/", "//")+"//"+Config.getCurrentLanguage()+"//"+"Inflection//";
+    public static final String allDelas = Config.getUserDir()+"/"+Config.getCurrentLanguage()+"/"+"Dela/Delas/";
+    public static final String allDelac = Config.getUserDir()+"/"+Config.getCurrentLanguage()+"/"+"Dela/Delac/";
     public static final String statisticsTmpPath = "statisticsTmp.xls";
     public static final String delasTmpPath = Config.getUnitexDir()+"//App//DelasTmp.dic";
     public static final String delafTmpPath = Config.getUnitexDir()+"//App//DelafTmp.dic";
