1accd6198ddbc33c932b27b134f7eace6c7b2132
Sofian El Guetibi <sofian.el-guetibi@edu.univ-eiffel.fr>
2022-05-04 13:24:06 +0200

    [fix #128] Resolve NullPointerException
    Resolve NullPointerException when clicking on Set File button (Explore Graph Path menu)

--- unitex/src/fr/umlv/unitex/config/Config.java
+++ unitex/src/fr/umlv/unitex/config/Config.java
@@ -570,6 +570,8 @@ public class Config {
         exploreGraphOutputDialogBox = new JFileChooser();
         PersonalFileFilter txtFilter =new PersonalFileFilter("txt",
                                           "Text Files");
+        if (sentenceDialogBox == null)
+            sentenceDialogBox = getSentenceDialogBox();
         sentenceDialogBox.addChoosableFileFilter(txtFilter);
         sentenceDialogBox.setFileFilter(txtFilter);
         exploreGraphOutputDialogBox.setDialogType(JFileChooser.OPEN_DIALOG);
